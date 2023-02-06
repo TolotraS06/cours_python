715a7fde2d178f0bd0888b3f09b5940b158fe5cb
TolotraS06 <117970660+TolotraS06@users.noreply.github.com>
2023-02-06 11:06:53 +0300

    Machine learnia part 1

--- python_cours.py
+++ python_cours.py
@@ -9,13 +9,6 @@ Created on Mon Jan 30 14:41:52 2023
 from tkinter import *
 from random import randrange
 
-def drawline ():
-    "trace une ligne dans le canevas"
-    global x1,y1,x2,y2,coul
-    can1.create_line (x1,y1,x2,y2,width=2, fill=coul)
-    
-    y2,y1 = y2 + 10,y1-10
-
 
 def changecolor ():
     "changement aleatoire de la couleur du trace"
@@ -23,6 +16,15 @@ def changecolor ():
     pal= ['purple','cyan','maroon','green','red','blue','orange','yellow']
     c = randrange(8)
     coul = pal [c]
+    
+ 
+def drawline ():
+    "trace une ligne dans le canevas"
+    global x1,y1,x2,y2,coul
+    can1.create_line (x1,y1,x2,y2,width=2, fill=coul)    
+    y2,y1 = y2 + 10,y1-10
+    changecolor()
+
     
     
 x1,y1,x2,y2 = 10,190,190,10
@@ -39,8 +41,8 @@ bou1 = Button (fen1, text = 'Quitter', command = fen1.quit)
 bou1.pack (side=BOTTOM)
 bou2 = Button (fen1, text = 'tracer une ligne', command = drawline)
 bou2.pack ()
-bou3 = Button (fen1, text = 'autre couleur', command = changecolor)
-bou3.pack ()
+#bou3 = Button (fen1, text = 'autre couleur', command = changecolor)
+#bou3.pack ()
 fen1.mainloop()
 
 fen1.destroy ()
